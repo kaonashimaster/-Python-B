5d93920e2b0274dd29efb04f0711308d0568354a
kaonashimaster <a.hisa06nori05tamon@gmail.com>
2026-03-17 16:43:19 +0900

    課題1終了。全体の流れを作成

---
+++ puzmon1.py
@@ -0,0 +1,20 @@
+'''
+作成日: 2026/03/17
+作成者: 相坂尚成
+'''
+#インポート
+
+#グローバル変数宣言
+
+#関数宣言
+def main():
+    player_name = input("プレイヤー名を入力してください> ")
+
+    print("***Puzzle & Monsters***")
+    print(f"{player_name}はダンジョンに到着した")
+    print(f"{player_name}はダンジョンを制覇した")
+    print("***GAME CLEARED!!***")
+
+    print("倒したモンスター数=5")
+#main関数呼び出し
+main()
